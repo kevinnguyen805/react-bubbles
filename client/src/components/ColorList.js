bde579e6b9695b3530ea97a542dc72eb8bf57cd2
admin <admin@admins-MacBook-Pro.local>
2019-10-18 09:38:36 -0700

    successful edit call

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import {axiosWithAuth} from '../utils/axiosWithAuth'
 
 const initialColor = {
   color: "",
@@ -18,14 +19,18 @@ const ColorList = ({ colors, updateColors, history }) => {
   };
 
   console.log(history)
+  console.log(colorToEdit)
+  
 
   const saveEdit = e => {
     e.preventDefault();
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
-
-    
+    axiosWithAuth().put(`/api/colors/${colorToEdit.id}`, colorToEdit)
+    .then(response => {
+      console.log(response)
+    })
   };
 
   const deleteColor = color => {
