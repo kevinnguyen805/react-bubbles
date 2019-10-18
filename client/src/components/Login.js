031dc39683cc47631fbfbd0e0c8ff5bf2a591768
admin <admin@admins-MacBook-Pro.local>
2019-10-18 08:06:14 -0700

    initial commit

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [credentials, setCredentials] = useState({
+    usename:'',
+    password:''
+  })
+
+
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
